3df6df6b050eb464fe9c09c99d49abc194c57ad2
Kendall <61364252+kmsherrin@users.noreply.github.com>
2024-06-09 16:30:14 +1200

    whoops

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
                     t.src=v;s=b.getElementsByTagName(e)[0];
                     s.parentNode.insertBefore(t,s)}(window, document,'script',
                     'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '{your-pixel-id-goes-here}');
+                    fbq('init', '1257494181782435');
                     fbq('track', 'PageView');
                   </script>
                   <noscript>
